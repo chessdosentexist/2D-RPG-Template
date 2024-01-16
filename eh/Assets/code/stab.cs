b0d00d648db54793abddfbc44e55370b5b264052
chessdosentexist <144922161+chessdosentexist@users.noreply.github.com>
2024-01-16 09:59:21 -0500

    start of melee thing

--- eh/Assets/code/stab.cs
+++ eh/Assets/code/stab.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class stab : MonoBehaviour
 {
     public float damage;
-    // Start is called before the first frame update
-    void Start()
+    public GameObject player;
+    public GameObject self;
+    private void Awake()
     {
-        
+        player = GameObject.Find("Good Guy");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (new Vector2(self.transform.position.x-player.transform.position.x, self.transform.position.y - player.transform.position.y).magnitude <= 0.5f)
+        {
+            player.GetComponent<HealthCalculations>().take_damage(damage);
+        }
     }
 }
